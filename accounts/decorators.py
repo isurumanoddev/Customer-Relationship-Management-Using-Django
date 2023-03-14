a90f68b9b00c287e504f5c00dc251023c6d2c4a3
isuru <isurumanod99@gmail.com>
2023-03-14 05:47:55 +0530

    add allowed roles decorater

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -16,7 +16,15 @@ def allowed_user(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
 
-
+            group = None
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+                print(group)
+            if group in allowed_roles:
+                print("user allowed")
+                return view_func(request, *args, **kwargs)
+            else:
+                return HttpResponse("<h1>user Not allowed </h1>")
 
 
 
