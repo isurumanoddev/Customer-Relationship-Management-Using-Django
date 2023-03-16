ac8bfa037286c1d77000315f279a4797834ea5f3
isuru <isurumanod99@gmail.com>
2023-03-16 17:53:48 +0530

    email chainged

--- CRM/settings.py
+++ CRM/settings.py
@@ -130,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'isurumanoddev@gmail.com'
-EMAIL_HOST_PASSWORD = 'zwvpleidsumzdtmu'
+EMAIL_HOST_USER = 'webdevisuru@gmail.com'
+EMAIL_HOST_PASSWORD = 'pcqzompnqlhcqdts'
 
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
