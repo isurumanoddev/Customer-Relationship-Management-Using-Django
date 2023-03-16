a967ee6a7a708367dcaa4655c7c1c5f79c31ea20
isuru <isurumanod99@gmail.com>
2023-03-16 09:24:19 +0530

    set urls for password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
 from accounts import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -21,5 +21,13 @@ urlpatterns = [
     path('user_login/', views.user_login, name="login"),
     path('user_logout/', views.user_logout, name="logout"),
     path('user_register/', views.user_register, name="register"),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(),name="reset_password"),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view() ,name="password_reset_confirm"),
+
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
 
 ]
